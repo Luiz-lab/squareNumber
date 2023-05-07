3ec860858a914ae93261cb4bbb9d76f7b6982a0c
Luiz-lab <calixtoluiz0@gmail.com>
2023-05-07 12:23:32 -0300

    Initial Setup

---
+++ index.test.js
@@ -0,0 +1,7 @@
+const { squareNumber } = require("./index");
+
+describe("Numerical Test", () => {
+  test("Input 0", () => {});
+  const Input = 0;
+  expect(squareNumber(Input)).tobe("false");
+});
