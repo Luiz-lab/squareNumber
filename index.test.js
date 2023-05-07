0bca7800f090f5ffdf91981984e3e12309a12874
Luiz Calixto <60370522+Luiz-lab@users.noreply.github.com>
2023-05-07 14:57:56 -0300

    Atualizar o index.test.js input - 1

--- index.test.js
+++ index.test.js
@@ -4,4 +4,7 @@ describe("Numerical Test", () => {
   test("Input 0", () => {});
   const Input = 0;
   expect(squareNumber(Input)).tobe("true");
+  test ("Input - 1", () => {});
+  const Input = - 1;
+  expect(squareNumber(Input)).tobe("false");
 });
