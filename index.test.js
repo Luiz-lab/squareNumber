d37c84c4f82f3bd19c4ce5b6d42737656f10e15d
Luiz Calixto <60370522+Luiz-lab@users.noreply.github.com>
2023-05-07 14:53:44 -0300

    Atualizar o index.test.js

--- index.test.js
+++ index.test.js
@@ -3,5 +3,5 @@ const { squareNumber } = require("./index");
 describe("Numerical Test", () => {
   test("Input 0", () => {});
   const Input = 0;
-  expect(squareNumber(Input)).tobe("false");
+  expect(squareNumber(Input)).tobe("true");
 });
